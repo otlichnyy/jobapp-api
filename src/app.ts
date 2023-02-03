04a27c20ad3d8dd94f115276565c599028708216
otlichnny <12508523+otlichnyy@users.noreply.github.com>
2023-02-03 17:32:31 +0530

    feat: :sparkles: add error handler to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import * as httpStatus from 'http-status';
 import { successLogHandler, errorLogHandler } from './middleware/morgan';
+import { customErrorHandler, errorConvertor } from './middleware/errorHandler';
+import ApiError from './utils/apiError';
 
 const app = express();
 
@@ -12,5 +15,16 @@ if (process.env.NODE_ENV !== 'test') {
 app.get('/healthz', (_, res) => {
   res.send({ status: 'OK' });
 });
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// Convert any other Error type to APIError
+app.use(errorConvertor);
+
+// Handle app error
+app.use(customErrorHandler);
 
 export default app;
